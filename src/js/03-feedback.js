6facb8a6732ccd34d144deee7e5581a06c0dea52
AndriiKovalchuk17 <113931911+AndriiKovalchuk17@users.noreply.github.com>
2023-03-20 11:31:19 +0200

    fix

---
+++ src/js/03-feedback.js
@@ -0,0 +1,46 @@
+import { throttle } from 'lodash';
+
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const massage = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener(
+    'input',
+    throttle(e => {
+        const objectToSave = { email: email.value, message: message.value };
+        
+        localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectToSave));
+    }, 500)
+);
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    if (email.value === '' || message.value === '') {
+        return alert('Заповніть всі поля!');
+    }
+
+    console.log({ email: email.value, message: message.value });
+
+    form.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+});
+
+
+const load = key => {
+    try {
+        const serializedState = localStorage.getItem(key);
+
+        return serializedState === null ? undefined : JSON.parse(serializedState);
+    } catch(error) {
+        console.error('Get State error: ', error.massage);
+    }
+};
+
+const strogeData = load(LOCALSTORAGE_KEY);
+
+if (strogeData) {
+    email.value = strogeData.email;
+    message.value = strogeData.message;
+};
